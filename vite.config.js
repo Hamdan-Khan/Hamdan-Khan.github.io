1e1506d1e94bdf31b950a66e7a38011e32c6fe6a
Hamdan <hamdankhan212@gmail.com>
2023-06-28 14:37:14 +0500

    Converted into PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        includeAssets: ["favicon.ico", "apple-touch-icon.png", "assets/*"],
+        name: "Hamdan K. | Developer",
+        short_name: "Hamdan K. Dev",
+        description:
+          "Full-Stack Freelance Website Developer having expertise in JavaScript frameworks, React.js, and Python.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable_icon.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
+      },
+    }),
+  ],
+});
